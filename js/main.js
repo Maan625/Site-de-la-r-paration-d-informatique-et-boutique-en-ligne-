df406b4320ac6644f5af681a09484b9390189256
AL ALI <mz777778@gmail.com>
2025-10-24 12:59:48 +0200

    je fais une list hamburger

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ let btn = document.getElementById("button_list");
+ let nav_list = document.getElementById("nav_list");
+ let div_link = document.getElementById("navbarToggleExternalContent");
+ 
+ window.addEventListener("click", (e) => {
+     if (e.target !== btn && e.target !== nav_list) {
+        div_link.classList.remove("show");
+      }
+ });
+ 
+//  btn.addEventListener("click", () => {
+//      div_link.classList.toggle("show");
+//  });

c65031b456a942dedb93990ea173ac9978c5c9fc
AL ALI <mz777778@gmail.com>
2025-11-10 11:57:23 +0100

    add la page nouveau compte

--- js/main.js
+++ js/main.js
@@ -3,34 +3,7 @@ const div_link = document.getElementById("navbarToggleExternalContent");
 const image_site = document.getElementById("image_de_site");
 const input_search = document.getElementById("input_search");
 
-// btn.addEventListener("click", (e) => {
-//   e.stopPropagation();
-//   image_site.style.opacity = "0";
-//   div_link.classList.toggle("show");
-//    input_search.style.backgroundColor = "red";
-// });
-
-// window.addEventListener("click", (e) => {
-//   const clickDansMenu = div_link && div_link.contains(e.target);
-//   const clickDansBouton = btn && btn.contains(e.target);
-
-//   if (!clickDansMenu && !clickDansBouton) {
-//     div_link.classList.remove("show");
-//     image_site.style.opacity = "1";
-//   }
-// });
-
-//version mobile
-// if(window.matchMedia("(max-width: 600px)").matches){
-//     btn.addEventListener("click", () => {
-//         image_site.style.opacity = "0";
-//     });
-// }
-// else{
-//     window.addEventListener("click", () => {
-//         image_site.style.opacity = "1";
-//     });
-// }
+ 
 btn.addEventListener("click", () => {
   // image_site.style.opacity = "0";
   div_link.classList.toggle("show");
@@ -47,3 +20,17 @@ window.addEventListener("click", (e) => {
   
   }
 })
+
+const formaulaire = document.getElementsByClassName("form_contact");
+const formaulaire_deja = document.getElementsByClassName("form_contact_deja");
+ const checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    formaulaire[0].style.display = "none";
+    formaulaire_deja[0].style.display = "block";
+  } else {
+    formaulaire[0].style.display = "block";
+    formaulaire_deja[0].style.display = "none";
+  }
+});
